fcc3734dc7e38e79b0f26cfbbf1f26e1e559978d
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2021-08-22 17:44:08 -0700

    Added Comments for readability

--- Snake/src/game.h
+++ Snake/src/game.h
@@ -7,6 +7,7 @@
 
 #include <GLFW/glfw3.h>
 
+// Direction the snake is moving
 enum Direction
 {
 	UP,
@@ -14,7 +15,7 @@ enum Direction
 	LEFT,
 	RIGHT
 };
-
+// Key Options to move, pause and restart
 enum KeyOptions
 {
 	KEY_W             = GLFW_KEY_W, 
@@ -40,37 +41,48 @@ public:
 	void processInput();			// Store user input
 	void update(float dt);			// Update Game Events
 	void render();					// Render the Game
-	void removeCells(Cell cell);
-	void spawnObstacle();
-	void restart();
-	void updateDimensions(int width, int height);
-	void endGame();
+	void removeCells(Cell cell);	// Remove empty cell
+	void spawnObstacle();			// Spawn an obstacle
+	void restart();					// Restart the game on r press
+	void updateDimensions(int width, int height);	// Update game dimensions 
+	void endGame();					// Clear memory
 private:
+	// Check Snake position
 	Cell      snakeLastCell;
 	Cell      snakeCurrentCell;
+
+	// Check Snake Direction
 	Direction oldDirection;
 	Direction currentDirection;
 
+	// Create renderer and food object
 	Renderer* renderer;
 	GameObject* food;
+	// Vectors to add snake obstacles and empty cells
 	std::vector<Cell>       emptyCells;
 	std::vector<GameObject> snake;
 	std::vector<GameObject> obstacles;
 
+	// Check keys processed
 	int keysProcessed[10] = { KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_W, KEY_S, KEY_A, KEY_D, KEY_P, KEY_R };
+
+	// Positional variables
 	int width;
 	int height;
 	int tileX;
 	int tileY;
 
+	// Resolution variables
 	float resolution;
 	float cellWidth;
 	float cellHeight;
 
+	// Game update score, timer, and speed of snake
 	unsigned int score = 0;
 	float stepTimer = 0;
 	float snakeSpeed = 8.0f;
 
+	// Check for gaame over or pause
 	bool gamePaused;
 	bool gameOver;
 
